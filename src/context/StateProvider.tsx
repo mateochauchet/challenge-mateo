ff7b326d90a3125016d06a7260c87e8eac6b5e01
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-11-23 19:40:26 -0300

    implement initial items request

--- src/context/StateProvider.tsx
+++ src/context/StateProvider.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { ItemsContext } from "./ItemsContext";
 import { IItem } from "../entities";
-import { mockItems } from "../mocks/items.mocks";
 
 export const ItemsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [items, setItems] = useState<IItem[]>(mockItems);
+  const [items, setItems] = useState<IItem[]>([]);
 
   const state = { items };
   const actions = { setItems };
--- src/wrappers/Items/ItemsWrapper.tsx
+++ src/wrappers/Items/ItemsWrapper.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { ItemsList } from "../../components";
+import { useItems } from "../../hooks/useItems";
 
 const ItemsWrapper = () => {
+  const { requestTables } = useItems();
+
+  useEffect(() => {
+    requestTables();
+  }, []);
+
   return (
     <div>
       <ItemsList />
