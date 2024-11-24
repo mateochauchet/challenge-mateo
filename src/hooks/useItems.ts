ea9e8dc9ba7f322200a6ac8166f2556786f76cf6
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-11-23 22:45:38 -0300

    implment infinite scroll

--- src/hooks/useItems.ts
+++ src/hooks/useItems.ts
@@ -2,15 +2,28 @@ import { useItemsContext } from "../context/ItemsContext";
 import itemService from "../services/item.service";
 
 const useItems = () => {
-  const { actions } = useItemsContext();
-  const { setItems } = actions;
+  const {
+    state: { items },
+    actions,
+  } = useItemsContext();
+  const { setItems, setLoading, setPage, setHasNext } = actions;
 
-  const requestTables = async () => {
+  const requestTables = async (params?: Record<string, any>) => {
     try {
-      const items = await itemService.getItems();
-      setItems(items);
+      setLoading(true);
+      const newItems = await itemService.getItems(params);
+
+      if (!newItems.length) {
+        setHasNext(false);
+        return;
+      }
+
+      setItems([...items, ...newItems]);
+      setPage(params?._page || 1);
     } catch (error) {
       console.error("Error fetching items:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
--- src/wrappers/Items/ItemsWrapper.tsx
+++ src/wrappers/Items/ItemsWrapper.tsx
@@ -1,12 +1,28 @@
 import { useEffect } from "react";
 import { ItemsList } from "../../components";
 import { useItems } from "../../hooks/useItems";
+import { useScroll } from "../../hooks/useScroll";
+import { useItemsContext } from "../../context/ItemsContext";
 
 const ItemsWrapper = () => {
+  const { state } = useItemsContext();
+  const {
+    pagination: { currentPage, hasNext },
+  } = state;
   const { requestTables } = useItems();
 
+  const handleOnBottom = () => {
+    if (hasNext) {
+      requestTables({ _page: currentPage + 1 });
+    }
+  };
+
+  useScroll({
+    onBottom: handleOnBottom,
+  });
+
   useEffect(() => {
-    requestTables();
+    requestTables({ _page: 1 });
   }, []);
 
   return (
