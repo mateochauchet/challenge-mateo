2032cb11f523d61a936906097ae1e202db687ffc
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-11-23 22:43:06 -0300

    add useScroll hook

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+interface IProps {
+  onBottom: () => void;
+}
+
+export const useScroll = ({ onBottom }: IProps) => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight;
+
+      if (isBottom) {
+        onBottom();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [onBottom]);
+};
