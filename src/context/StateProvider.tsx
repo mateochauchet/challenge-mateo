e32c0bb7804920763e124bef66a8beee552c7c3d
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-11-23 22:44:23 -0300

    add loading & pagination to context

--- src/context/StateProvider.tsx
+++ src/context/StateProvider.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import { ItemsContext } from "./ItemsContext";
 import { IItem } from "../entities";
+import { IState, ItemsActions } from "./types";
 
 export const ItemsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [items, setItems] = useState<IItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currentPage, setPage] = useState(1);
+  const [hasNext, setHasNext] = useState(true);
 
-  const state = { items };
-  const actions = { setItems };
+  const state: IState = {
+    items,
+    loading,
+    pagination: { currentPage, hasNext },
+  };
+  const actions: ItemsActions = { setItems, setLoading, setPage, setHasNext };
 
   return (
     <ItemsContext.Provider value={{ state, actions }}>
--- src/context/types.ts
+++ src/context/types.ts
@@ -2,8 +2,18 @@ import { IItem } from "../entities";
 
 export interface IState {
   items: IItem[];
+  pagination: IPagination;
+  loading: boolean;
+}
+
+interface IPagination {
+  currentPage: number;
+  hasNext: boolean;
 }
 
 export interface ItemsActions {
   setItems: (items: IItem[]) => void;
+  setLoading: (loading: boolean) => void;
+  setPage: (page: number) => void;
+  setHasNext: (hasNext: boolean) => void;
 }
